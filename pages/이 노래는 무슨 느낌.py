5afe7337bae9355b66635b73d686620c7ff63702
jaesekim <jaeseki@gmail.com>
2022-07-25 05:18:11 +0900

    test

--- pages/이 노래는 무슨 느낌.py
+++ pages/이 노래는 무슨 느낌.py
@@ -1,2 +1,15 @@
 import streamlit as st
+import tensorflow as tf
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Conv1D, Embedding, Bidirectional, LSTM, Dense, Dropout, BatchNormalization
+from tensorflow.python.keras.callbacks import EarlyStopping
 from contents.contents import *
+
+st.markdown("# Write lyrics!")
+st.markdown("### How does this song feel?")
+st.markdown("##### You can predict more appropriately by writing lyrics \
+            that are 15 bits or more with fewer other things (such as spaces).")
+lyrics = st.text_input("Write down lyrics over 15bits. More ")
+
+model = tf.keras.models.load_model("./data/lstm_model.h5")
+st.write(model.summary())
