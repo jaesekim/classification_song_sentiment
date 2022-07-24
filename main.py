6eb1709e7fdd73a5fae47309f483150d5a5ff0cf
jaesekim <jaeseki@gmail.com>
2022-07-24 23:31:59 +0900

    1

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.markdown('# NLP Mid Project')
+st.markdown('## 노래 가사 감정 분류')
+st.markdown("노래 가사를 올리면 어느 감정의 노래인지 분류해 줍니다!")
+st.markdown('* 데이터 셋 출처')
+st.markdown('** (https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp')
+st.markdown('** (https://www.kaggle.com/pashupatigupta/emotion-detection-from-text)')
+
+
+st.markdown('---')
+
+st.markdown("팀장: 양조은")
+st.markdown("팀원: 김재석, 이성진, 신희철")
