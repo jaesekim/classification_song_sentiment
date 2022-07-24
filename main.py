2f73156a569cc5c489a39618c82ca65d6775d493
jaesekim <jaeseki@gmail.com>
2022-07-24 23:38:01 +0900

    test1

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import streamlit as st
 
 st.markdown('# NLP Mid Project')
 st.markdown('## 노래 가사 감정 분류')
-st.markdown("노래 가사를 올리면 어느 감정의 노래인지 분류해 줍니다!")
-st.markdown('* 데이터 셋 출처')
-st.markdown('** https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp')
-st.markdown('** https://www.kaggle.com/pashupatigupta/emotion-detection-from-text')
+st.markdown("### 노래 가사를 올리면 어느 감정의 노래인지 분류해 줍니다!")
+st.markdown('##### 데이터 셋 출처')
+st.markdown('* https://www.kaggle.com/praveengovi/emotions-dataset-for-nlp')
+st.markdown('* https://www.kaggle.com/pashupatigupta/emotion-detection-from-text')
 
 
 st.markdown('---')
